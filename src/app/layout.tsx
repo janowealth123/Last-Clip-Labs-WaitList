3083df9e2221928fb912389dd281337159650042
janowealth123 <janospadafora@gmail.com>
2025-01-28 07:07:48 -0500

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'ClipLabs - Get Paid to Create Viral Clips',
+  description: 'Clip for Top Influencers And Brands– Get Paid to Create',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
